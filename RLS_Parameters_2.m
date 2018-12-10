dbc5ba8ece3f63d53bd1edadfa4c303855db6ce2
ktran95 <43077250+ktran95@users.noreply.github.com>
2018-12-10 16:26:03 -0500

    Add files via upload

--- RLS_Parameters_2.m
+++ RLS_Parameters_2.m
@@ -1,5 +1,6 @@
 clc 
 clear
+load('data.mat');
 time = data(:,1);
 SOC = data(:,2)/100;
 OCV = data(:,3);
@@ -16,16 +17,21 @@ a1(1) = T/(C1(1)*R1(1))-1;
 lambda = 0.97;
 Umod(1) = U(1);
 theta = [b0(1); b1(1); a1(1); OCV(1)];
-P = abs(diag(0.1*theta));
+P = abs(diag(0.01*theta));
+OCV_mod(1) = OCV(1);
+eocv(1) = 0;
 for i = 2:n 
     phi = [I(i); I(i-1); (OCV(i-1)-U(i-1)); 1];
     Umod(i) = transpose(theta)*phi;
     L = (P*phi)/(lambda + transpose(phi)*P*phi);
+    %P = (P-L*transpose(phi)*P)/lambda;
     P = (1/lambda)*(eye(4)-L*transpose(phi))*P;
     theta = theta + L*(U(i) - transpose(phi)*theta);
     b0(i) = theta(1);
     b1(i) = theta(2);
     a1(i) = theta(3);
+    OCV_mod(i) = theta(4);
+    eocv(i) = OCV(i) - OCV_mod(i);
     R0(i) = b0(i);
     R1(i) = (b1(i)-a1(i)*b0(i))/(1+a1(i));
     C1(i) = T/(b1(i)-a1(i)*b0(i)); 
